4cdc546ddff12d7812a897e4a5956a00aeef0fff
cshekar2k10 <cshekar2k10@gmail.com>
2023-02-07 18:40:24 +0530

    Updating learn-shell

--- 06-special-vars.sh
+++ 06-special-vars.sh
@@ -1,6 +1,19 @@
+echo "following values from script"
 echo "0 - $0"
 echo "1 - $1"
 echo "2 - $2"
 echo "* - $*"
 echo "@ - $@"
 echo "# - $#"
+
+example() {
+  echo "following values from function"
+echo "0 - $0"
+echo "1 - $1"
+echo "2 - $2"
+echo "* - $*"
+echo "@ - $@"
+echo "# - $#"
+}
+
+example
